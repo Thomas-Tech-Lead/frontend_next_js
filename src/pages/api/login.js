0384d27967efb2a15b198de0cb2fd806f8a0152b
Thomas <thomastigistuad@gmail.com>
2024-01-12 04:29:04 -0800

    update 1

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,6 +1,6 @@
 export default function login(req, res) {
     const body = req.body
-    if (body.Phone === "251944719460" && body.Password === "password") {
+    if ((body.Phone === "user" || body.Phone === "User") && (body.Password === "password" || body.Password === "Password") ) {
         res.status(200).json({ 
             message: "true"
         })
