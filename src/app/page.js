2a4f6e97fcaef206f05152d260a84d60af61d870
Thomas <thomastigistuad@gmail.com>
2024-01-12 04:41:14 -0800

    update 2

--- src/app/page.js
+++ src/app/page.js
@@ -57,8 +57,9 @@ export default function Home() {
                             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                                 <h1> <slot>{input}</slot> </h1>
                                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                    Login to your account
+                                    Login to your account  
                                 </h1>
+                                Your Phone = user <br/> Password = password
 
                                 <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" method="post">
                                     <div>
